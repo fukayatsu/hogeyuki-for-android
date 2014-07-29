24aa9ad537b60793789b37c0f0b9ab2a51143bd5
fukayatsu <fukayatsu@gmail.com>
2014-07-29 23:45:41 +0900

    Add getting image function

--- app/src/main/java/com/fukayatsu/hogeyuki/MainActivity.java
+++ app/src/main/java/com/fukayatsu/hogeyuki/MainActivity.java
@@ -1,17 +1,28 @@
 package com.fukayatsu.hogeyuki;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
+    static final int REQUEST_GALLERY = 990;
+    static final int REQUEST_CAMERA  = 991;
+
+    Uri mImageUri;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView  = (ImageView) findViewById(R.id.imageView);
     }
 
 
@@ -28,9 +39,46 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        Intent intent = null;
+        switch (item.getItemId()) {
+            case R.id.action_camera:
+                String fileName = "hogeyuki_new_photo.jpg";
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, fileName);
+                values.put(MediaStore.Images.Media.DESCRIPTION,"Image capture by camera");
+                mImageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                intent = new Intent();
+                intent.setAction("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+                startActivityForResult(intent, REQUEST_CAMERA);
+                return true;
+            case R.id.action_gallery:
+                intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select picture"), REQUEST_GALLERY);
+                return true;
+            case R.id.action_reset:
+                return true;
+            case R.id.action_send:
+                return true;
+            case R.id.action_save:
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_GALLERY) {
+                mImageUri = intent.getData();
+            } else if (requestCode == REQUEST_CAMERA) {
+                if (mImageUri == null) { return ; }
+            }
+            mImageView.setImageURI(mImageUri);
+
+        }
     }
 }
